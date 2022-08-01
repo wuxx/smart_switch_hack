415396efb5f648cd62c9d5d865d52fd5640fb78f
wuxx <wujianhua0924@gmail.com>
2022-08-01 14:08:28 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   ../example/esp32c3-vl53l0x/Makefile
    	modified:   ../example/esp32c3-vl53l0x/firmware/bootloader.bin
    	modified:   ../example/esp32c3-vl53l0x/firmware/vl53l0x_example.bin
    	modified:   flash_erase.sh
    	deleted:    flash_write_esp8266_tasmota.sh
    	new file:   smartchip/flash_write_esp8266_tasmota.sh
    	new file:   smartchip/tasmota-CN.bin

---
+++ image/smartchip/flash_write_esp8266_tasmota.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+
+#SERIAL_PORT=/dev/ttyS0
+#SERIAL_PORT=/dev/ttyUSB0
+SERIAL_PORT=/dev/ttyACM0
+
+BAUDRATE=115200
+#BAUDRATE=460800
+#BAUDRATE=576000
+
+#32Mbit/4MByte
+esptool.py --port ${SERIAL_PORT} \
+        --baud ${BAUDRATE} write_flash \
+        -fs 4MB -ff 80m  \
+        0x00000  tasmota-CN.bin
